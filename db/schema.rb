eea11cb450da481f5d3b092951ac26e83908b557
Rick Graham <rick@raddstudio.com>
2017-05-07 23:53:32 -0300

    add retailers scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507220101) do
+ActiveRecord::Schema.define(version: 20170508022821) do
 
   create_table "products", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "retailers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
